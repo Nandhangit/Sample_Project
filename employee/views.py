9bd03cf0f9cb5df060c8dcc9badeb9bb10ca0e8e
Nandhangit <snandhan825@gmail.com>
2026-05-08 16:10:20 +0530

    first commit

---
+++ employee/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Employee
+from .serializers import EmployeeSerializer
+
+class EmployeeListCreateView(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class EmployeeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticated]
